82f21f713eaca69d35b2ce7a85717568b13b93e5
keveeen <105822487+keveeen@users.noreply.github.com>
2022-09-30 19:12:32 +0200

    commit

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
 
 
